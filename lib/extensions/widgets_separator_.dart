aa1e742b9afd73d3f125168bc37739d12b792ced
jafar <jafarrezazadeh76@gmail.com>
2024-10-09 23:57:28 +0330

    documents added to (widgets_Separator)

--- lib/extensions/widgets_separator_.dart
+++ lib/extensions/widgets_separator_.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
+/// [WidgetsSeparator]Extension on List<Widget> to separate widgets with a gap or divider.
 extension WidgetsSeparator on List<Widget> {
+  /// [withGapInBetween] Returns a new list with gaps of the specified size between each widget.
+  ///
+  /// If the list is empty, an empty list is returned.
+  ///
+  /// Example:
+  /// ```dart
+  /// List<Widget> widgets = [Widget1(), Widget2(), Widget3()];
+  /// List<Widget> separatedWidgets = widgets.withGapInBetween(10.0);
+  /// ```
   List<Widget> withGapInBetween(double gapSize) {
     List<Widget> separatedList = <Widget>[];
 
@@ -21,6 +31,23 @@ extension WidgetsSeparator on List<Widget> {
   bool _isNotFirstAndLastItem(Widget e) =>
       indexOf(e) > 0 && indexOf(e) < length;
 
+  /// [withDividerInBetween] Returns a new list with dividers between each widget.
+  ///
+  /// The divider's color, height, thickness, indent, and endIndent can be customized.
+  ///
+  /// If the list is empty, an empty list is returned.
+  ///
+  /// Example:
+  /// ```dart
+  /// List<Widget> widgets = [Widget1(), Widget2(), Widget3()];
+  /// List<Widget> separatedWidgets = widgets.withDividerInBetween(
+  ///   color: Colors.grey,
+  ///   height: 1.0,
+  ///   thickness: 1.0,
+  ///   indent: 10.0,
+  ///   endIndent: 10.0,
+  /// );
+  /// ```
   List<Widget> withDividerInBetween({
     Color? color,
     double? height,
